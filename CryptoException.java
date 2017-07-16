1f7346120e78a110c403d9c41149e56ed547da43
suremann <ssherman@iastate.edu>
2017-07-15 20:04:37 -0500

    Update CryptoException.java

--- CryptoException.java
+++ CryptoException.java
@@ -1,5 +1,9 @@
-package stuff2;
-
+package passwordmanager;
+/**
+ * A utility class that encrypts or decrypts a file.
+ * @author www.codejava.net
+ *
+ */
 public class CryptoException extends Exception {
 	 
 	private static final long serialVersionUID = 1L;
